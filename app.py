b550b7217e533079bbdee660de029f622d62b11c
Sagnik Sarkar <sagnik106@gmail.com>
2021-01-31 02:00:11 +0530

    final changes dev phase

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from datetime import datetime
+from datetime import date, datetime, timedelta
 from flask import Flask, request, Response, render_template
 from flask_cors import CORS
 import sqlite3
+from support.db_init import init
 
+init()
 app = Flask(__name__)
 
 def getim():
@@ -41,10 +43,45 @@ def dange():
 def pies():
     conn = sqlite3.connect("support/data.db")
     a=conn.execute("SELECT COUNT(*) FROM pos WHERE BACK=? AND DTIME LIKE ? ORDER BY DTIME", ("False", str(datetime.now())[:10]+"%")).fetchall()[0][0]
-    t=conn.execute("SELECT COUNT(*) FROM pos").fetchall()[0][0]
+    t=conn.execute("SELECT COUNT(*) FROM pos WHERE DTIME LIKE ? ORDER BY DTIME", (str(datetime.now())[:10]+"%",)).fetchall()[0][0]
     conn.close()
     return "%.2f"%(a*100/t)
 
+@app.route('/lnf', methods=['POST', 'GET'])
+def lnfs():
+    d=date.today()
+    td=timedelta(days=1)
+    al=list()
+    tl=list()
+    dl=list()
+    conn=sqlite3.connect("support/data.db")
+    tot=conn.execute("SELECT COUNT(*) FROM pos").fetchall()[0][0]
+    while True:
+        al.append(conn.execute("SELECT COUNT(*) FROM pos WHERE BACK=? AND DTIME LIKE ? ORDER BY DTIME", ("False", str(d)+"%")).fetchall()[0][0])
+        tl.append(conn.execute("SELECT COUNT(*) FROM pos WHERE DTIME LIKE ? ORDER BY DTIME", (str(d)+"%",)).fetchall()[0][0])
+        dl.append(str(d))
+        d=d-td
+        if sum(tl)>=tot:
+            break
+    conn.close()
+    return {'val': [i*100/j for i,j in zip(al, tl)], 'dates': dl}
+
+@app.route('/ltemp', methods=['POST', 'GET'])
+def ltemps():
+    te=list()
+    ti=list()
+    conn=sqlite3.connect('support/data.db')
+    l=conn.execute("SELECT * FROM temp ORDER BY DTIME").fetchall()
+    conn.close()
+    return {
+        'x': [i for i, _ in l],
+        'y': [i for _, i in l]
+    }
+@app.route('/gtemp', methods=['POST', 'GET'])
+def gtemps():
+    conn=sqlite3.connect('support/data.db')
+    a=conn.execute("SELECT TEM FROM temp ORDER BY DTIME").fetchall()[0][0]
+    return str(a)
 
 if __name__=="__main__":
     app.run(debug=True)
--- static/JS/stats.js
+++ static/JS/stats.js
@@ -1,103 +1,238 @@
-var trace1 = {
-    x: [1, 2, 3, 4],
-    y: [10, 15, 13, 17],
-    type: 'scatter' 
-};
-var data1 = [trace1];
-var layout1 = {
-    shapes: [
-    {
-        type: 'line',
-        xref: 'paper',
-        x0: 0,
-        y0: 12.0,
-        x1: 1,
-        y1: 12.0,
-        line:{
-            color: 'rgb(255, 0, 0)',
-            width: 2,
-            dash:'dash'
-        }
-    }
-    ],
-    title: "Sampled Results", 
-    xaxis: {title: "Value"}, 
-    yaxis: {title: "Count"},
-    height: 498,
-    width: 889
-}
-let config1 = { 
-    responsive: true,
-    displaylogo: false,
-};
-Plotly.newPlot('myDiv1', data1, layout1, config1);
+// var trace1 = {
+//     x: [1, 2, 3, 4],
+//     y: [10, 15, 13, 17],
+//     type: 'scatter' 
+// };
+// var data1 = [trace1];
+// var layout1 = {
+//     shapes: [
+//     {
+//         type: 'line',
+//         xref: 'paper',
+//         x0: 0,
+//         y0: 12.0,
+//         x1: 1,
+//         y1: 12.0,
+//         line:{
+//             color: 'rgb(255, 0, 0)',
+//             width: 2,
+//             dash:'dash'
+//         }
+//     }
+//     ],
+//     title: "Sampled Results", 
+//     xaxis: {title: "Value"}, 
+//     yaxis: {title: "Count"},
+//     height: 498,
+//     width: 889
+// }
+// let config1 = { 
+//     responsive: true,
+//     displaylogo: false,
+// };
+// Plotly.newPlot('myDiv1', data1, layout1, config1);
 
-var data2 = [{
-    type: "pie",
-    values: [2, 3],
-    labels: ["Correct Position", "Incorrect Position"],
-    textinfo: "label+percent",
-    insidetextorientation: "radial"
-  }]
+// var data2 = [{
+//     type: "pie",
+//     values: [2, 3],
+//     labels: ["Correct Position", "Incorrect Position"],
+//     textinfo: "label+percent",
+//     insidetextorientation: "radial"
+//   }]
   
-  var layout2 = {
-    title: "Sleeping Position of the Baby",
-    height: 498,
-    width: 582
-  }
-  let config2 = { 
-    responsive: true,
-    displaylogo: false,
-};
-Plotly.newPlot('myDiv2', data2, layout2, config2);
+//   var layout2 = {
+//     title: "Sleeping Position of the Baby",
+//     height: 498,
+//     width: 582
+//   }
+//   let config2 = { 
+//     responsive: true,
+//     displaylogo: false,
+// };
+// Plotly.newPlot('myDiv2', data2, layout2, config2);
 
-var data3 = [
-	{
-		domain: { x: [0, 1], y: [0, 1] },
-		value: 270,
-		title: { text: "Temperature" },
-		type: "indicator",
-		mode: "gauge+number"
-	}
-];
+// var data3 = [
+// 	{
+// 		domain: { x: [0, 1], y: [0, 1] },
+// 		value: 270,
+// 		title: { text: "Temperature" },
+// 		type: "indicator",
+// 		mode: "gauge+number"
+// 	}
+// ];
 
-var layout3 = { width: 582, height: 498, margin: { t: 0, b: 0 } };
-let config3 = { 
-    responsive: true,
-    displaylogo: false,
-};
-Plotly.newPlot('myDiv3', data3, layout3, config3);
+// var layout3 = { width: 582, height: 498, margin: { t: 0, b: 0 } };
+// let config3 = { 
+//     responsive: true,
+//     displaylogo: false,
+// };
+// Plotly.newPlot('myDiv3', data3, layout3, config3);
 
-trace4 = {
-    type: 'scatter',
-    x: [1, 2, 3, 4],
-    y: [10, 15, 13, 17],
-    mode: 'lines',
-    name: 'Red',
-    line: {
-      color: 'rgb(219, 64, 82)',
-      width: 3
-    }
-  };
+// trace4 = {
+//     type: 'scatter',
+//     x: [1, 2, 3, 4],
+//     y: [10, 15, 13, 17],
+//     mode: 'lines',
+//     name: 'Red',
+//     line: {
+//       color: 'rgb(219, 64, 82)',
+//       width: 3
+//     }
+//   };
  
-  var layout4 = {
-    title: 'Quarter 1 Growth',
-    xaxis: {
-        title: 'GDP per Capita',
-        showgrid: false,
-        zeroline: false
-    },
-    yaxis: {
-        title: 'Percent',
-        showline: false
-    },
-    height: 498,
-    width: 889
-  };
+//   var layout4 = {
+//     title: 'Quarter 1 Growth',
+//     xaxis: {
+//         title: 'GDP per Capita',
+//         showgrid: false,
+//         zeroline: false
+//     },
+//     yaxis: {
+//         title: 'Percent',
+//         showline: false
+//     },
+//     height: 498,
+//     width: 889
+//   };
   
-  var data4 = [trace4];
-  let config4 = { 
-    responsive: true,
-    displaylogo: false,
+//   var data4 = [trace4];
+//   let config4 = { 
+//     responsive: true,
+//     displaylogo: false,
+// };
+// Plotly.newPlot('myDiv4', data4, layout4, config4);
+
+
+function pieupdate(){
+  $.post('/pie', {}, (data, error)=>{
+    var data2 = [{
+        type: "pie",
+        values: [100-data, data],
+        labels: ["Correct Position", "Incorrect Position"],
+        textinfo: "label+percent",
+        insidetextorientation: "radial"
+      }]
+      
+      var layout2 = {
+        title: "Sleeping Position of the Baby (Today)",
+        // height: 498,
+        // width: 582
+      }
+      let config2 = { 
+        responsive: true,
+        displaylogo: false,
+    };
+    Plotly.newPlot('myDiv2', data2, layout2, config2);
+  });
+}
+
+function lnfupdate(){
+  $.post('/lnf', {}, (data, error)=>{
+    var trace1 = {
+        x: data.dates,
+        y: data.val,
+        type: 'scatter' 
+    };
+    var data1 = [trace1];
+    var layout1 = {
+        shapes: [
+        {
+            type: 'line',
+            xref: 'paper',
+            x0: 0,
+            y0: 15.0,
+            x1: 1,
+            y1: 15.0,
+            line:{
+                color: 'rgb(255, 0, 0)',
+                width: 2,
+                dash:'dash'
+            }
+        }
+        ],
+        title: "Daily Wrong Position Percentage", 
+        xaxis: {title: "Date"}, 
+        yaxis: {title: "Percentage"},
+        // height: 498,
+        // width: 889
+    }
+    let config1 = { 
+        responsive: true,
+        displaylogo: false,
+    };
+    Plotly.newPlot('myDiv1', data1, layout1, config1);
+  });
+}
+function gtempupdate(){
+  $.post('/gtemp', {}, (data, error)=>{
+    var data3 = [
+      {
+        domain: { x: [0, 1], y: [0, 1] },
+        value: data,
+        title: { text: "Latest Temperature Reading" },
+        type: "indicator",
+        mode: "gauge+number"
+      }
+    ];
+    
+    var layout3 = { 
+      // width: 582, 
+      // height: 498, 
+      margin: { 
+        t: 0, 
+        b: 0 
+      } 
+    };
+    let config3 = { 
+        responsive: true,
+        displaylogo: false,
+    };
+    Plotly.newPlot('myDiv3', data3, layout3, config3);
+  });
+}
+
+function ltempupdate(){
+  $.post('/ltemp', {}, (data, error)=>{
+    trace4 = {
+      type: 'scatter',
+      x: data.x,
+      y: data.y,
+      mode: 'lines',
+      name: 'Red',
+      line: {
+        color: 'rgb(219, 64, 82)',
+        width: 3
+      }
+    };
+   
+    var layout4 = {
+      title: "Baby's temperature monitor",
+      xaxis: {
+          title: "Date - Time",
+          showgrid: false,
+          zeroline: false
+      },
+      yaxis: {
+          title: 'Temperature in Fahrenheit',
+          showline: false
+      },
+      // height: 498,
+      // width: 889
+    };
+    
+    var data4 = [trace4];
+    let config4 = { 
+      responsive: true,
+      displaylogo: false,
+  };
+  Plotly.newPlot('myDiv4', data4, layout4, config4);
+  });
+}
+allup=()=>{
+  pieupdate();
+  lnfupdate();
+  ltempupdate();
+  gtempupdate();
 };
-Plotly.newPlot('myDiv4', data4, layout4, config4);
+
+setInterval(allup, 3000);
